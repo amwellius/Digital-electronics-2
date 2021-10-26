1cfb39db23df8056150e1b3cd37143fe429002c1
amwellius <samino21@gmail.com>
2021-10-26 21:40:52 +0200

    added 06 C files

---
+++ Labs/06-lcd/06-lcd/06-lcd/lcd.h
@@ -0,0 +1,16 @@
+/*
+ * lcd.h
+ *
+ * Created: 26. 10. 2021 21:37:30
+ *  Author: samin
+ */ 
+
+
+#ifndef LCD_H_
+#define LCD_H_
+
+
+
+
+
+#endif /* LCD_H_ */
---
+++ Labs/06-lcd/06-lcd/06-lcd/lcd_definitions.h
@@ -0,0 +1,16 @@
+/*
+ * lcd_definitions.h
+ *
+ * Created: 26. 10. 2021 21:37:44
+ *  Author: samin
+ */ 
+
+
+#ifndef LCD_DEFINITIONS_H_
+#define LCD_DEFINITIONS_H_
+
+
+
+
+
+#endif /* LCD_DEFINITIONS_H_ */
