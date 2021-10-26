007fc2bf6873c6d27c04863bf5b67a15d563e59d
amwellius <samino21@gmail.com>
2021-10-26 20:58:00 +0200

    created 06 lab

---
+++ Labs/06-lcd/06-lcd/06-lcd/main.c
@@ -0,0 +1,18 @@
+/*
+ * 06-lcd.c
+ *
+ * Created: 26. 10. 2021 20:57:32
+ * Author : samin
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
