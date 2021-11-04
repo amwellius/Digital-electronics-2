b7f70bfe73cedf09d9d5453a7cdb30b91f203b6c
amwellius <samino21@gmail.com>
2021-11-04 20:24:35 +0100

    added 07 files

--- Labs/07-uart/07-uart/07-uart/main.c
+++ Labs/07-uart/07-uart/07-uart/main.c
@@ -25,6 +25,30 @@
  *           per second. Send value to LCD and UART.
  * Returns:  none
  **********************************************************************/
+
+/** @brief  Size of the circular receive buffer, must be power of 2
+ *
+ *  You may need to adapt this constant to your target and your application by adding
+ *  CDEFS += -DUART_RX_BUFFER_SIZE=nn to your Makefile.
+ */
+#ifndef UART_RX_BUFFER_SIZE
+# define UART_RX_BUFFER_SIZE 64
+#endif
+
+/** @brief  Size of the circular transmit buffer, must be power of 2
+ *
+ *  You may need to adapt this constant to your target and your application by adding
+ *  CDEFS += -DUART_TX_BUFFER_SIZE=nn to your Makefile.
+ */
+#ifndef UART_TX_BUFFER_SIZE
+#define UART_TX_BUFFER_SIZE 64
+#endif
+
+#ifndef F_CPU
+#define F_CPU 16000000
+#endif
+
+
 int main(void)
 {
     // Initialize LCD display
@@ -37,14 +61,25 @@ int main(void)
 
     // Configure ADC to convert PC0[A0] analog value
     // Set ADC reference to AVcc
-
+    ADMUX = ADMUX | (1<<REFS0);
+    ADMUX = ADMUX &~ (1<<REFS1);        //0
+    
     // Set input channel to ADC0
-
+    ADMUX = ADMUX &~ (1<<MUX3);
+    ADMUX = ADMUX &~ (1<<MUX2);
+    ADMUX = ADMUX &~ (1<<MUX1);
+    ADMUX = ADMUX &~ (1<<MUX0);
+    
     // Enable ADC module
-
+    ADCSRA = ADCSRA | (1<<ADEN);
+    
     // Enable conversion complete interrupt
+    ADCSRA = ADCSRA | (1<<ADIE);
 
     // Set clock prescaler to 128
+    ADCSRA = ADCSRA | (1<<ADPS2);
+    ADCSRA = ADCSRA | (1<<ADPS1);
+    ADCSRA = ADCSRA | (1<<ADPS0);
     
 
     // Configure 16-bit Timer/Counter1 to start ADC conversion
@@ -53,6 +88,7 @@ int main(void)
     TIM1_overflow_interrupt_enable();   
 
     // Initialize UART to asynchronous, 8N1, 9600
+    uart_init(UART_BAUD_SELECT(9600, F_CPU));
 
     // Enables interrupts by setting the global interrupt mask
     sei();
@@ -77,6 +113,14 @@ int main(void)
 ISR(TIMER1_OVF_vect)
 {
     // Start ADC conversion
+    /*
+    static uint8_t number_of_overflows = 0;
+    number_of_overflows++;
+    if (number_of_overflows >= 1)
+    {
+        */
+        ADCSRA = ADCSRA | (1<<ADSC);
+   // }    
 
 }
 
@@ -86,6 +130,51 @@ ISR(TIMER1_OVF_vect)
  **********************************************************************/
 ISR(ADC_vect)
 {
-    // WRITE YOUR CODE HERE
+    uint16_t value = 0;
+    char lcd_string[4] = "0000";
+    
+    //ADC je 16 bitova hodnota ktoru on precita naraz
+    value = ADC;                  // Copy ADC result to 16-bit variable
+    itoa(value, lcd_string, 10);  // Convert decimal value to string 
 
+    // A value displaying
+    lcd_gotoxy(8,0);
+    lcd_puts("    ");
+    lcd_gotoxy(8,0);     
+    lcd_puts(lcd_string);
+    //uart communication
+    uart_puts(lcd_string);
+    uart_puts("\r\n");    
+    
+    // B value
+    value = ADC;                  // Copy ADC result to 16-bit variable
+    itoa(value, lcd_string, 16);
+    
+    lcd_gotoxy(13,0);
+    //display clearing
+    lcd_puts("   ");
+    lcd_gotoxy(13,0);    
+    lcd_puts(lcd_string);
+    //uart communication
+    uart_puts(lcd_string);
+    uart_puts("\r\n");
+        
+    // C value
+    value = ADC;                  // Copy ADC result to 16-bit variable
+    itoa(value, lcd_string, 2);
+    lcd_gotoxy(8,1);
+    lcd_puts("        ");
+    //uart communication
+    uart_puts(lcd_string);
+    uart_puts("\r\n");
+    
+    switch(value){
+        case 0 ... 90       : lcd_gotoxy(8,1); lcd_puts("right"); uart_puts("right"); uart_puts("\r\n");  break;        //0
+        case 91 ... 200     : lcd_gotoxy(8,1); lcd_puts("up"); uart_puts("up"); uart_puts("\r\n");  break;           //99
+        case 201 ... 400    : lcd_gotoxy(8,1); lcd_puts("down"); uart_puts("down"); uart_puts("\r\n");  break;         //255
+        case 401 ... 600    : lcd_gotoxy(8,1); lcd_puts("left"); uart_puts("left"); uart_puts("\r\n");  break;         //408
+        case 601 ... 1000   : lcd_gotoxy(8,1); lcd_puts("select"); uart_puts("select"); uart_puts("\r\n");  break;       //638
+        case 1001 ... 1050  : lcd_gotoxy(8,1); lcd_puts("none"); uart_puts("none"); uart_puts("\r\n");  break;         //1023
+        default:lcd_puts("       ");
+    }      
 }
